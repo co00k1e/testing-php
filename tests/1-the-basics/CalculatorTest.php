bd3e2d82a981ac3c519f5757a3eb97292b427c5c
co00k1e <cookiespenceremail@gmail.com>
2026-03-10 10:52:25 +0000

    TP-2: Adds sub and setup method and

--- tests/1-the-basics/CalculatorTest.php
+++ tests/1-the-basics/CalculatorTest.php
@@ -5,10 +5,23 @@ use App\Calculator;
 
 class CalculatorTest extends TestCase
 {
+
+    private Calculator $calculator;
+
+    protected function setUp(): void
+    {
+        $this->calculator = new Calculator();
+    }
+
     public function testSum()
     {
-        $calculator = new Calculator();
-        $result = $calculator->sum(2, 3);
+        $result = $this->calculator->sum(2, 3);
         $this->assertEquals(5, $result);
+    }
+
+    public function testSub()
+    {
+        $result = $this->calculator->sub(5, 3);
+        $this->assertEquals(2, $result);
     }
 }
